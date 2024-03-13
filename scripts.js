84b6ce548fa91a4595d65db4e5b7bded09ab66da
MarlonMuller <72674862+MarlonMuller@users.noreply.github.com>
2024-03-12 22:06:42 -0300

    update 12.03.2024

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const myLi = document.querySelector("#myLis")
+
+myLi.innerHTML = `
+        <li>
+            <img src="assets/bacon-egg.png" alt="burger bacon egg">
+            <p id="name">X-salada</p>
+            <p id="price">R$ 30</p>
+        </li>`
